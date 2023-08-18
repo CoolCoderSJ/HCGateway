e8488b90abd27c457700ce4a7fc975e95c6f523b
Shuchir Jain <53063247+CoolCoderSJ@users.noreply.github.com>
2023-08-18 15:58:16 -0400

    com 2

--- ios/HCGatewayTests/HCGatewayTests.m
+++ ios/HCGatewayTests/HCGatewayTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface SMHealthWebTests : XCTestCase
+@interface HCGatewayTests : XCTestCase
 
 @end
 
-@implementation SMHealthWebTests
+@implementation HCGatewayTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
